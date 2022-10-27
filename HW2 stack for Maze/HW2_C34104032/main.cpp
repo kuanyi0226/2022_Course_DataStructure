a56a8a978dfbe5acea537192dafa96e1795606ce
kuanyi0226 <kuanyi0226@gmail.com>
2022-10-27 22:42:38 +0800

    HW2 fix

--- HW2 stack for Maze/HW2_C34104032/main.cpp
+++ HW2 stack for Maze/HW2_C34104032/main.cpp
@@ -12,12 +12,15 @@ struct offsets{
 	int b;//col
 } ;
 enum directions{
-	E, SE, S, SW, W, NW, N, NE
+	O, E, SE, S, SW, W, NW, N, NE
 };
 
 struct Items{ //list of triple to record the pass history
 	int x, y, dir;
 	//Constructor
+	Items(){
+
+	}
 	Items(int i, int j, directions Dir){
 		x = i; y = j; dir = Dir;
 	}	
@@ -41,21 +44,22 @@ class Stack
 
 
 //declare moving offsets
-offsets moveDir[8];
+offsets moveDir[9]; 
 
 //declare maze array
 char maze[mazeRow][mazeCol];
 //initialize mark array
-int mark[mazeRow+2][mazeCol+2] = {0};	
+int mark[mazeRow][mazeCol] = {0};	
 
 void Path(const int, const int);
 
 int main(int argc, char** argv) {
 	//Initialize moving offsets
-	/*E*/moveDir[0].a = 0; moveDir[0].b = 1; /*SE*/moveDir[1].a = 1; moveDir[1].b = 1;
-	/*S*/moveDir[2].a = 1; moveDir[2].b = 0; /*SW*/moveDir[3].a = 1; moveDir[3].b = -1;
-	/*W*/moveDir[4].a = 0; moveDir[4].b = -1; /*NW*/moveDir[5].a = -1; moveDir[5].b = -1;
-	/*N*/moveDir[6].a = -1; moveDir[6].b = 0; /*NE*/moveDir[7].a = -1; moveDir[7].b = 1;	
+	/*O*/moveDir[0].a = 0; moveDir[0].b = 0;
+	/*E*/moveDir[1].a = 0; moveDir[1].b = 1; /*SE*/moveDir[2].a = 1; moveDir[2].b = 1;
+	/*S*/moveDir[3].a = 1; moveDir[3].b = 0; /*SW*/moveDir[4].a = 1; moveDir[4].b = -1;
+	/*W*/moveDir[5].a = 0; moveDir[5].b = -1; /*NW*/moveDir[6].a = -1; moveDir[6].b = -1;
+	/*N*/moveDir[7].a = -1; moveDir[7].b = 0; /*NE*/moveDir[8].a = -1; moveDir[8].b = 1;	
 
 	char inputName[40];
 	cout<< "enter filename : ";
@@ -71,25 +75,31 @@ int main(int argc, char** argv) {
 				input.get(c);
 				if(c != '\n'){
 					maze[i][j] = c;
-					cout<<maze[i][j];
+					cout<<maze[i][j];			
 				}
 				
 			}
 			cout<<endl;
-		}		
+		}			
 	}	
 	input.close();
 	
 	Path(mazeRow, mazeCol);
-	
+
 	return 0;
 }
 
 void Path(const int m, const int p){
-	//start at (1,0)
+	//Initialize: start at (1,0)
+	int counter = 0; //走了幾步
 	mark[1][0] = 1;
+	for(int i = 0; i<17; i++){
+		for(int j=0;j<17;j++)
+			cout<< mark[i][j];
+		cout<<endl;
+	}
 	Stack stack(m*p); //capacity of stack
-	Items temp(1,1,E); //set temp.x, temp.y, temp.dir
+	Items temp(1,0,E); //set temp.x, temp.y, temp.dir
 	stack.Push(temp);
 	
 	
@@ -98,26 +108,39 @@ void Path(const int m, const int p){
 		stack.Pop();//unstack
 		int i = temp.x; int j = temp.y; int d = temp.dir;
 		
-		while(d<8){//move forward
+		while(d<9){//move forward
 			int g = i + moveDir[d].a; int h = j + moveDir[d].b;
+			//cout<< g<< " "<< h<<" "<< temp.dir << endl;
+
+
 			if(g==m && h==p){//reach exit
+				
 				//output path
 				//cout<<stack.;
 				cout<<i<<" "<<j<<endl; // last two squares on the path
 				cout<< m << " " << p << endl;
+				cout<<"successfully escaped!!";
 				return;
 				
 			}
-			if((!maze[g][h]) && (!mark[g][h])){ //new position
-				mark[g][h] = 1;
+			if(!(maze[g][h]) && !(mark[g][h])){ //new position
+
+				mark[g][h] = 1;				
 				temp.x = i; temp.y = j; temp.dir = d+1;
+				//cout<< temp.x<< " "<< temp.y<<" "<< temp.dir << endl;
 				stack.Push(temp); //stack it
 				i = g; j =h; d = E; //East; move to (g, h)
+				
+
 			}
-			else d++; //try next dir
-		} 
-	} //End of while
-	cout<<"No path in maze"<<endl;
+			
+			else {
+				//cout<< "d++"<<endl;
+				d++; //try next dir
+			} 
+		} //End of finding dir
+	} //End of finding path
+	cout<<"Failed to escape."<<endl;
 	
 }//End of Path Func
 
@@ -126,7 +149,7 @@ void Path(const int m, const int p){
 Stack::Stack(int StackCapacity)
 {
 	stackCapacity = StackCapacity;
-	stack = (Items*)malloc(sizeof(Items) * stackCapacity); //include <cstdlib>
+	stack = new Items[stackCapacity];//(Items*)malloc(sizeof(Items) * stackCapacity); //include <cstdlib>
 	top = -1;
 }
 
@@ -146,9 +169,9 @@ void Stack::Push(const Items& x){
 	stack[++top] = x;
 }
 
-void Stack::Pop(){
+void Stack::Pop(){//
 	if(!IsEmpty()){
-		stack[top--].~Items();
+		top--;
 	}
 	
 }
